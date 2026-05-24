1d6512eccf2f6c7640bbb7287aa698689196eab4
Juan <naujralios01@gmail.com>
2026-05-24 15:32:25 -0600

    agregar sweetalert2 para mensaje de enviado... y limpiar imputs

--- src/app/components/formulario/formulario.ts
+++ src/app/components/formulario/formulario.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import Swal from 'sweetalert2';
+import { NgForm } from '@angular/forms';
 import {
   Firestore,
   collection,
@@ -26,8 +27,36 @@ export class FormularioComponent {
   color = '';
   seguroVida = '';
   codigo = this.generarCodigo();
+async guardarMototaxi(formulario: NgForm) {
 
-  async guardarMototaxi() {
+  // Eliminar espacios vacíos
+  const nombresLimpio = this.nombres.trim();
+  const apellidosLimpio = this.apellidos.trim();
+  const dpiLimpio = this.dpi.trim();
+  const domicilioLimpio = this.domicilio.trim();
+  const colorLimpio = this.color.trim();
+
+  // Validación
+  if (
+    !nombresLimpio ||
+    !apellidosLimpio ||
+    !dpiLimpio ||
+    !domicilioLimpio ||
+    !this.modelo ||
+    !colorLimpio ||
+    !this.seguroVida
+  ) {
+
+    Swal.fire({
+      icon: 'warning',
+      title: 'Campos vacíos',
+      text: 'Debes completar todos los campos',
+      confirmButtonColor: '#4f46e5'
+    });
+
+    return;
+
+  }
 
   try {
 
@@ -35,41 +64,44 @@ export class FormularioComponent {
 
     await addDoc(referencia, {
 
-      nombres: this.nombres,
-      apellidos: this.apellidos,
-      dpi: this.dpi,
-      domicilio: this.domicilio,
+      nombres: nombresLimpio,
+      apellidos: apellidosLimpio,
+      dpi: dpiLimpio,
+      domicilio: domicilioLimpio,
       modelo: this.modelo,
-      color: this.color,
+      color: colorLimpio,
       seguroVida: this.seguroVida,
       codigo: this.codigo
 
     });
 
-    console.log("Mototaxi registrado");
+    // Mensaje bonito
+    Swal.fire({
+      icon: 'success',
+      title: 'Mototaxi registrado',
+      text: 'La información fue guardada correctamente',
+      confirmButtonColor: '#4f46e5'
+    });
 
     // Limpiar formulario
-    this.nombres = '';
-    this.apellidos = '';
-    this.dpi = '';
-    this.domicilio = '';
-    this.modelo = '';
-    this.color = '';
-    this.seguroVida = '';
+    formulario.resetForm();
 
     // Nuevo código automático
     this.codigo = this.generarCodigo();
-
-    return;
 
   } catch (error) {
 
     console.log(error);
 
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'No se pudo registrar el mototaxi'
+    });
+
   }
 
 }
-
   generarCodigo(): string {
 
     const numero = Math.floor(100 + Math.random() * 900);
