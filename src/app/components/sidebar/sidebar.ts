e8183581c5595596c7e38cf04492779a103633b8
Juan <naujralios01@gmail.com>
2026-05-24 09:30:11 -0600

    estrcuturar archivo y logica de sidebar

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,14 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './sidebar.html',
 })
 export class SidebarComponent {
 
+  // =========================================================
+  // RECIBE EL ESTADO DEL SIDEBAR DESDE EL LAYOUT
+  // =========================================================
+  @Input() sidebarOpen = false;
+
+  // =========================================================
+  // ENVÍA EVENTO PARA CERRAR EL SIDEBAR
+  // =========================================================
   @Output() close = new EventEmitter<void>();
 
   closeSidebar() {
