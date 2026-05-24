63682475b2fc3b13eeaf716dce00164601928ccc
Juan <naujralios01@gmail.com>
2026-05-24 15:20:07 -0600

    agregar logica para guardar datos en firestore

--- src/app/components/formulario/formulario.ts
+++ src/app/components/formulario/formulario.ts
@@ -1,11 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import {
+  Firestore,
+  collection,
+  addDoc
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './formulario.html',
-  styleUrl: './formulario.css',
 })
-export class Formulario {
+
+export class FormularioComponent {
+
+  firestore = inject(Firestore);
+
+  nombres = '';
+  apellidos = '';
+  dpi = '';
+  domicilio = '';
+  modelo = '';
+  color = '';
+  seguroVida = '';
+  codigo = this.generarCodigo();
+
+  async guardarMototaxi() {
+
+  try {
+
+    const referencia = collection(this.firestore, 'mototaxis');
+
+    await addDoc(referencia, {
+
+      nombres: this.nombres,
+      apellidos: this.apellidos,
+      dpi: this.dpi,
+      domicilio: this.domicilio,
+      modelo: this.modelo,
+      color: this.color,
+      seguroVida: this.seguroVida,
+      codigo: this.codigo
+
+    });
+
+    console.log("Mototaxi registrado");
+
+    // Limpiar formulario
+    this.nombres = '';
+    this.apellidos = '';
+    this.dpi = '';
+    this.domicilio = '';
+    this.modelo = '';
+    this.color = '';
+    this.seguroVida = '';
+
+    // Nuevo código automático
+    this.codigo = this.generarCodigo();
+
+    return;
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+
+}
+
+  generarCodigo(): string {
+
+    const numero = Math.floor(100 + Math.random() * 900);
+
+    const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const letra1 = letras[Math.floor(Math.random() * 26)];
+    const letra2 = letras[Math.floor(Math.random() * 26)];
+    const letra3 = letras[Math.floor(Math.random() * 26)];
+
+    return `M ${numero} ${letra1}${letra2}${letra3}`;
+
+
+
+  }
 
 }
--- src/app/pages/registrar-propietarios/registrar-propietarios.ts
+++ src/app/pages/registrar-propietarios/registrar-propietarios.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Formulario} from '../../components/formulario/formulario';
+import { FormularioComponent } from '../../components/formulario/formulario';
 
 @Component({
   selector: 'app-registrar-propietarios',
-  imports: [Formulario],
+  imports: [FormularioComponent],
   templateUrl: './registrar-propietarios.html',
   styleUrl: './registrar-propietarios.css',
 })
