bf4aff4d10c56ef0ff4becd4dcc7ea447816700d
Juan <naujralios01@gmail.com>
2026-05-24 20:22:47 -0600

    comentar codigo de servide auth.ts

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,32 +1,64 @@
 import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 
+// Importaciones necesarias de Firebase Authentication
 import {
+
+  // Servicio principal de autenticación
   Auth,
+
+  // Método para iniciar sesión usando una ventana emergente
   signInWithPopup,
+
+  // Proveedor de autenticación con Google
   GoogleAuthProvider,
-   GithubAuthProvider,
+
+  // Proveedor de autenticación con GitHub
+  GithubAuthProvider,
+
+  // Método para cerrar sesión
   signOut
-  
+
 } from '@angular/fire/auth';
 
+
+// Hace que este servicio pueda usarse en toda la aplicación
 @Injectable({
   providedIn: 'root',
 })
 
 export class AuthService {
 
-  constructor(private auth: Auth) {}
 
+  //inyeccion del servicio de Auth de Firebase
+  autenticar = inject(Auth);
+
+
+
+  // Método para iniciar sesión con Google
   loginGoogle() {
-    return signInWithPopup(this.auth, new GoogleAuthProvider());
+
+    // Abre una ventana emergente para autenticarse con Google
+    return signInWithPopup(this.autenticar, new GoogleAuthProvider());
+
   }
 
 
+
+  // Método para iniciar sesión con GitHub
   loginGithub() {
-  return signInWithPopup(this.auth, new GithubAuthProvider());
-}
+
+    // Abre una ventana emergente para autenticarse con GitHub
+    return signInWithPopup(this.autenticar, new GithubAuthProvider());
+
+  }
+
+  // Método para cerrar sesión
   logout() {
-    return signOut(this.auth);
+
+    // Cierra la sesión del usuario actual
+    return signOut(this.autenticar);
+
   }
 
 }
