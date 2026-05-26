653eb3c9b13defe0d6c8f3dbb1c11ce584f1e305
Juan <naujralios01@gmail.com>
2026-05-26 09:36:38 -0600

    Se implementó módulo de edición con precarga de datos desde Firebase

--- src/app/pages/editar-propietarios/editar-propietarios.ts
+++ src/app/pages/editar-propietarios/editar-propietarios.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { MototaxiService } from '../../services/mototaxi';
 import { inject } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 
 
 @Component({
   selector: 'app-editar-propietarios',
+  standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './editar-propietarios.html',
   styleUrl: './editar-propietarios.css',
 })
 
-export class EditarPropietarios {
+export class EditarPropietarios implements OnInit{
 
   nombres = '';
   apellidos = '';
@@ -29,28 +31,57 @@ export class EditarPropietarios {
   motoService = inject(MototaxiService);
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cdRef: ChangeDetectorRef
   ) {}
 
-  async ngOnInit() {
+  // async ngOnInit() {
 
-    this.id =
-      this.route.snapshot.queryParamMap.get('id')!;
+  //   this.id =
+  //     this.route.snapshot.queryParamMap.get('id')!;
 
-    const data =
-      await this.motoService.obtenerPorId(this.id);
+  //   const data =
+  //     await this.motoService.obtenerPorId(this.id);
 
-    this.nombres = data.nombres;
-    this.apellidos = data.apellidos;
-    this.dpi = data.dpi;
-    this.domicilio = data.domicilio;
-    this.modelo = data.modelo;
-    this.color = data.color;
-    this.seguroVida = data.seguroVida;
-    this.codigo = data.codigo;
+  //   this.nombres = data.nombres;
+  //   this.apellidos = data.apellidos;
+  //   this.dpi = data.dpi;
+  //   this.domicilio = data.domicilio;
+  //   this.modelo = data.modelo;
+  //   this.color = data.color;
+  //   this.seguroVida = data.seguroVida;
+  //   this.codigo = data.codigo;
 
-  }
+  // }
 
+
+
+async ngOnInit() {
+
+  console.log('SI ENTRA');
+
+  this.id =
+    this.route.snapshot.queryParamMap.get('id')!;
+
+  console.log(this.id);
+
+  const data =
+    await this.motoService.obtenerPorId(this.id);
+
+  console.log(data);
+
+  this.nombres = data.nombres;
+  this.apellidos = data.apellidos;
+  this.dpi = data.dpi;
+  this.domicilio = data.domicilio;
+  this.modelo = data.modelo;
+  this.color = data.color;
+  this.seguroVida = data.seguroVida;
+  this.codigo = data.codigo;
+
+  this.cdRef.detectChanges();
+
+}
 
   async actualizarMototaxi() {
 
